29fe2c9c6b24f2f011475ee9d7a6dddfe9fde779
Shantanu Bala <shantanubala@gmail.com>
2022-07-14 14:58:16 -0700

    format code

--- Package.swift
+++ Package.swift
@@ -4,34 +4,35 @@
 import PackageDescription
 
 let package = Package(
-    name: "MintingKit",
-    platforms: [
-        .iOS(.v14),
-        .macOS(.v10_15),
-        .watchOS(.v6),
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MintingKit",
-            targets: ["MintingKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/stleamist/BetterSafariView", from: "2.4.0"),
-        .package(name: "KeychainSwift", url: "https://github.com/evgenyneu/keychain-swift", from: "20.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.1"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MintingKit",
-            dependencies: ["BetterSafariView", "KeychainSwift", "Alamofire", "SwiftyJSON"]),
-        .testTarget(
-            name: "MintingKitTests",
-            dependencies: ["MintingKit"]),
-    ]
+  name: "MintingKit",
+  platforms: [
+    .iOS(.v14),
+    .macOS(.v10_15),
+    .watchOS(.v6),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "MintingKit",
+      targets: ["MintingKit"])
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/stleamist/BetterSafariView", from: "2.4.0"),
+    .package(
+      name: "KeychainSwift", url: "https://github.com/evgenyneu/keychain-swift", from: "20.0.0"),
+    .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.1"),
+    .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "MintingKit",
+      dependencies: ["BetterSafariView", "KeychainSwift", "Alamofire", "SwiftyJSON"]),
+    .testTarget(
+      name: "MintingKitTests",
+      dependencies: ["MintingKit"]),
+  ]
 )
--- Sources/MintingKit/MintingKit.swift
+++ Sources/MintingKit/MintingKit.swift
@@ -1,111 +1,125 @@
-import SwiftUI
-import BetterSafariView
-import LocalAuthentication
-import KeychainSwift
 import Alamofire
+import BetterSafariView
+import KeychainSwift
+import LocalAuthentication
+import SwiftUI
 import SwiftyJSON
 
-
 private enum LoginError: Error {
-    case tokenError(String)
+  case tokenError(String)
 }
 
 public struct ABProject {
-    let id: String
-    let title: String
+  let id: String
+  let title: String
 }
-
 
 public struct MintingKit {
-    let token: String
-    let endpoint = URL(string: "https://minting-api.artblocks.io")!
-    
-    private func buildHeaders() -> HTTPHeaders {
-        return [
-            "Authorization": "Token \(token)",
-            "Accept": "application/json"
-        ]
-    }
-    
-    public func listProjects(onSuccess: @escaping ([ABProject]) -> (), onFailure: @escaping (Error) -> ()) {
-        DispatchQueue.main.async {
-            AF.request(endpoint.appendingPathComponent("project"), method: .get, headers: buildHeaders()).validate().responseJSON { response in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)["results"].arrayValue
-                    let projects = json.map { project in
-                        return ABProject(id: project["id"].stringValue, title: project["title"].stringValue)
-                    }
-                    onSuccess(projects)
-                case .failure(let error):
-                    onFailure(error)
-                }
+  let token: String
+  let endpoint = URL(string: "https://minting-api.artblocks.io")!
+
+  private func buildHeaders() -> HTTPHeaders {
+    return [
+      "Authorization": "Token \(token)",
+      "Accept": "application/json",
+    ]
+  }
+
+  public func listProjects(
+    onSuccess: @escaping ([ABProject]) -> Void, onFailure: @escaping (Error) -> Void
+  ) {
+    DispatchQueue.main.async {
+      AF.request(endpoint.appendingPathComponent("project"), method: .get, headers: buildHeaders())
+        .validate().responseJSON { response in
+          switch response.result {
+          case .success(let value):
+            let json = JSON(value)["results"].arrayValue
+            let projects = json.map { project in
+              return ABProject(id: project["id"].stringValue, title: project["title"].stringValue)
             }
+            onSuccess(projects)
+          case .failure(let error):
+            onFailure(error)
+          }
         }
     }
+  }
 }
 
-public struct MintingLoginButton: View {
-    @State private var startingWebAuthenticationSession = false
-    let keychain = KeychainSwift()
-    let label = "Sign in"
-    let onSuccess: (String) -> ()
-    let onFailure: (Error) -> ()
-    
-    public var body: some View {
-        Button(label) {
-            startingWebAuthenticationSession = true
-        }.onAppear {
-            DispatchQueue.main.async {
-                guard let lastOpened = UserDefaults.standard.object(forKey: "LastOpened") as? Date else {
-                    return
-                }
-                guard let elapsed = Calendar.current.dateComponents([.day], from: lastOpened, to: Date()).day else {
-                    return
-                }
-                if elapsed >= 6 {
-                    return
-                }
-                if let t = keychain.get("authToken") {
-                    let context = LAContext()
-                    var error: NSError?
-                    
-                    // check whether biometric authentication is possible
-                    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-                        // it's possible, so go ahead and use it
-                        let reason = "We need to unlock your data."
-                        
-                        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                            // authentication has now completed
-                            if success {
-                                onSuccess(t)
-                            } else {
-                                // there was a problem
-                            }
-                        }
-                    } else {
-                        // no biometrics
-                    }
-                }
-            }
+public struct MintingLoginButton<Label: View>: View {
+  @State private var startingWebAuthenticationSession = false
+  let keychain = KeychainSwift()
+  let label: Label
+  let onSuccess: (String) -> Void
+  let onFailure: (Error) -> Void
+
+  init(
+    onSuccess: @escaping (String) -> Void, onFailure: @escaping (Error) -> Void,
+    @ViewBuilder label: () -> Label
+  ) {
+    self.label = label()
+    self.onSuccess = onSuccess
+    self.onFailure = onFailure
+  }
+
+  public var body: some View {
+    Button(action: { startingWebAuthenticationSession = true }) {
+      label
+    }.onAppear {
+      DispatchQueue.main.async {
+        guard let lastOpened = UserDefaults.standard.object(forKey: "LastOpened") as? Date else {
+          return
         }
-        .webAuthenticationSession(isPresented: $startingWebAuthenticationSession) {
-            WebAuthenticationSession(
-                url: URL(string: "https://minting-api.artblocks.io/app/?appauth=true")!,
-                callbackURLScheme: "txlessauth"
-            ) { callbackURL, error in
-                if let t = callbackURL?.host {
-                    DispatchQueue.main.async {
-                        keychain.set(t, forKey: "authToken")
-                        UserDefaults.standard.set(Date(), forKey: "LastOpened")
-                    }
-                    onSuccess(t)
-                } else if let error = error {
-                    onFailure(error)
-                } else {
-                    onFailure(LoginError.tokenError("Unable to retrieve token from login screen."))
-                }
-            }
+        guard
+          let elapsed = Calendar.current.dateComponents([.day], from: lastOpened, to: Date()).day
+        else {
+          return
         }
+        if elapsed >= 6 {
+          return
+        }
+        if let t = keychain.get("authToken") {
+          let context = LAContext()
+          var error: NSError?
+
+          // check whether biometric authentication is possible
+          if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(
+              .deviceOwnerAuthenticationWithBiometrics, localizedReason: reason
+            ) { success, authenticationError in
+              // authentication has now completed
+              if success {
+                onSuccess(t)
+              } else {
+                // there was a problem
+              }
+            }
+          } else {
+            // no biometrics
+          }
+        }
+      }
     }
+    .webAuthenticationSession(isPresented: $startingWebAuthenticationSession) {
+      WebAuthenticationSession(
+        url: URL(string: "https://minting-api.artblocks.io/app/?appauth=true")!,
+        callbackURLScheme: "txlessauth"
+      ) { callbackURL, error in
+        if let t = callbackURL?.host {
+          DispatchQueue.main.async {
+            keychain.set(t, forKey: "authToken")
+            UserDefaults.standard.set(Date(), forKey: "LastOpened")
+          }
+          onSuccess(t)
+        } else if let error = error {
+          onFailure(error)
+        } else {
+          onFailure(LoginError.tokenError("Unable to retrieve token from login screen."))
+        }
+      }
+    }
+  }
 }
